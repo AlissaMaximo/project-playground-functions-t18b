7dddef7f2bd2e5fff01e359be19de9aeee0d7ae6
AlissaMaximo <alissahiratamaximo@gmail.com>
2021-11-16 18:49:26 -0300

    Requisito 10 feito.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,16 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
   // seu código aqui
+  newArr = [];
+  if (array == false) {
+    return "Vazio!";
+  }
+
+  array.sort();
+  for (let i = 0; i < array.length; i += 1) {
+    newArr.push({tech: array[i], name: name});
+  }
+  return newArr;
 }
 
 // Desafio 11
@@ -24,3 +34,8 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+/*
+  Desafio 10 precisou do sort() para ordenar alfabeticamente os elementos. Fonte: https://www.w3schools.com/jsref/jsref_sort.asp
+
+*/
